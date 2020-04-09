11600489c13e0fc50c0e89bbd179addd54ddfadd
Sloutmyv <clercsyl9123@gmail.com>
2020-04-09 18:37:25 +1100

    20200409-1837 - Try Django 1.9 12/38

--- src/syc_blog_2020/urls.py
+++ src/syc_blog_2020/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', views.posts_home, name="posts"),
 ]
