aac92c9977a0a819ad38def077c7738e21668747
Sloutmyv <clercsyl9123@gmail.com>
2020-04-09 18:00:44 +1100

    20200409-1800 - Try Django 1.9 8/38

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title","timestamp","updated"]
+    list_filter = ["updated"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostAdmin)
