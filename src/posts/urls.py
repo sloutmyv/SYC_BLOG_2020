531fd7fa22346aa40174bfc89821f13f1f7d754b
Sloutmyv <clercsyl9123@gmail.com>
2020-04-09 19:47:34 +1100

    20200409-1947 - class based view implementation

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 from .views import (
-    posts_list,
+#    posts_list,
     posts_create,
     posts_delete,
     posts_detail,
     posts_update,
+    PostListView,
     )
 
 urlpatterns = [
+    path('',PostListView.as_view(), name='posts-list'),
     path('create/', posts_create),
     path('detail/', posts_detail),
-    path('', posts_list),
+    # path('', posts_list),
     path('update/', posts_update),
     path('delete/', posts_delete),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
 from .models import Post
 
 def posts_create(request):
@@ -11,13 +12,22 @@ def posts_detail(request):
     }
     return render(request, "base.html", context)
 
-def posts_list(request):
-    queryset = Post.objects.all()
-    context = {
-        "object_list": queryset,
-        "title" : "List",
-    }
-    return render(request, "base.html", context)
+class PostListView(ListView):
+    template_name = "base.html"
+    model = Post
+    paginate_by = 10
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+# def posts_list(request):
+#     queryset = Post.objects.all()
+#     context = {
+#         "object_list": queryset,
+#         "title" : "List",
+#     }
+#     return render(request, "base.html", context)
 
 def posts_update(request):
     return HttpResponse("<h1>update</h1>")
